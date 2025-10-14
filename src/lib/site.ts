50271031f6182532aeb291cc3846e694e7884b36
boogiemanders <chanders0@yahoo.com>
2025-10-13 20:58:55 -0400

    Update site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  name: "SkyAgent",
+  name: "thePsychology.ai",
   url: "https://agent-magicui.vercel.app",
-  description: "Your template for building AI-powered agents with Magic UI.",
+  description: "Pass the EPPP quickly by studying with accurate, optimized, and affordable AI-powered agents.",
   links: {
     twitter: "https://x.com/dillionverma",
     github: "https://github.com/dillionverma",
@@ -9,3 +9,4 @@ export const siteConfig = {
 };
 
 export type SiteConfig = typeof siteConfig;
+
