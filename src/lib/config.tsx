ce8e4971249a6146dbd86e66ad03c4eb46f217ae
boogiemanders <chanders0@yahoo.com>
2025-10-16 18:57:55 -0400

    Update config.tsx

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -83,21 +83,21 @@ export const siteConfig = {
       },
       {
         id: 2,
-        content: <SecondBentoAnimation />,
+        content: <FirstBentoAnimation />,
         title: "Clear Learning",
         description:
           "Dense EPPP content can become clear, relatable lessons using pop culture, metaphors, and humor according to your preferences.",
       },
       {
         id: 3,
-        content: <ThirdBentoAnimation />,
+        content: <FirstBentoAnimation />,
         title: "The Only Prep That Cares About Your Mindset",
         description:
           "Most programs only throw information at you and neglect burnout and stress. This program provides reminders for breaks and psychoeducation about ACT, MI, and CBT-I to rebuild focus while learning relevant EPPP topics.",
       },
       {
         id: 4,
-        content: <FourthBentoAnimation />,
+        content: <FirstBentoAnimation />,
         title: "Integrated studying",
         description: "Study anywhere with a mobile-friendly platform.",
       },
