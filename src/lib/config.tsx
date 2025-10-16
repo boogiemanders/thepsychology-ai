df39efe7a824a795b81295718d30c5020e6ecb3f
boogiemanders <chanders0@yahoo.com>
2025-10-16 17:55:58 -0400

    Update config.tsx
    chatgpt please fix

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -61,90 +61,98 @@ export const siteConfig = {
 
   // ---------- UPDATED LOGOS (black in light, white in dark) ----------
   companyShowcase: {
-    title: "Born from clinical and/or academic experiences with:",
-    companyLogos: [
-      {
-        id: 1,
-        name: "UCLA Health",
-        logo: (
-          <>
-            <img src="/ucla.png" alt="UCLA Health David Geffen School of Medicine" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/ucla-white.png" alt="UCLA Health David Geffen School of Medicine" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 2,
-        name: "NYU Langone Health",
-        logo: (
-          <>
-            <img src="/nyu.png" alt="NYU Langone Health" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/nyu-white.png" alt="NYU Langone Health" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 3,
-        name: "Montefiore Einstein",
-        logo: (
-          <>
-            <img src="/monte.png" alt="Montefiore Einstein" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/montefiore-einstein-white.png" alt="Montefiore Einstein" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 4,
-        name: "Pratt Institute",
-        logo: (
-          <>
-            <img src="/pratt.png" alt="Pratt Institute" className="h-10 w-auto object-contain block dark:hidden" />
-            <img src="/pratt-white.png" alt="Pratt Institute" className="h-10 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 5,
-        name: "LIU Post",
-        logo: (
-          <>
-            <img src="/liu.png" alt="LIU Post" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/liu-post-white.png" alt="LIU Post" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 6,
-        name: "Brooklyn Center for Psychotherapy",
-        logo: (
-          <>
-            <img src="/bcp.png" alt="Brooklyn Center for Psychotherapy" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/bcp-logo-white.png" alt="Brooklyn Center for Psychotherapy" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 7,
-        name: "NotebookLM",
-        logo: (
-          <>
-            <img src="/notebook.png" alt="NotebookLM" className="h-12 w-auto object-contain block dark:hidden" />
-            <img src="/notebooklm-white.png" alt="NotebookLM" className="h-12 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-      {
-        id: 8,
-        name: "OpenAI",
-        logo: (
-          <>
-            <img src="/openai.png" alt="OpenAI" className="h-6 w-auto object-contain block dark:hidden" />
-            <img src="/openai-white.png" alt="OpenAI" className="h-6 w-auto object-contain hidden dark:block" />
-          </>
-        ),
-      },
-    ],
-  },
+  title: "Born from clinical and/or academic experiences with:",
+  companyLogos: [
+    {
+      id: 1,
+      name: "UCLA Health",
+      logo: (
+        <img
+          src="/ucla.png"
+          alt="UCLA Health David Geffen School of Medicine"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 2,
+      name: "NYU Langone Health",
+      logo: (
+        <img
+          src="/nyu.png"
+          alt="NYU Langone Health"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 3,
+      name: "Montefiore Einstein",
+      logo: (
+        <img
+          src="/monte.png"
+          alt="Montefiore Einstein"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 4,
+      name: "Pratt Institute",
+      logo: (
+        <img
+          src="/pratt.png"
+          alt="Pratt Institute"
+          className="h-10 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 5,
+      name: "LIU Post",
+      logo: (
+        <img
+          src="/liu.png"
+          alt="LIU Post"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 6,
+      name: "Brooklyn Center for Psychotherapy",
+      logo: (
+        <img
+          src="/bcp.png"
+          alt="Brooklyn Center for Psychotherapy"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 7,
+      name: "NotebookLM",
+      logo: (
+        <img
+          src="/notebook.png"
+          alt="NotebookLM"
+          className="h-12 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+    {
+      id: 8,
+      name: "OpenAI",
+      logo: (
+        <img
+          src="/openai.png"
+          alt="OpenAI"
+          className="h-6 w-auto object-contain dark:invert"
+        />
+      ),
+    },
+  ],
+},
   // -------------------------------------------------------------------
 
   bentoSection: {
