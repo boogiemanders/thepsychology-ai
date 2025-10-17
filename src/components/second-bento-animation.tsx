8c3f6702d920e36db9337e70ea0c7ad27c19a147
boogiemanders <chanders0@yahoo.com>
2025-10-16 21:36:23 -0400

    Update second-bento-animation.tsx

--- src/components/second-bento-animation.tsx
+++ src/components/second-bento-animation.tsx
@@ -28,7 +28,7 @@ When Tony Stark tells the press “I am Iron Man,” he’s owning his truth. No
   );
 }
 
-export function FirstBentoAnimation() {
+export function SecondBentoAnimation() {
   const ref = useRef(null);
   const isInView = useInView(ref);
   const [shouldAnimate, setShouldAnimate] = useState(false);
@@ -149,5 +149,6 @@ export function FirstBentoAnimation() {
   );
 
 }
+
 
 
